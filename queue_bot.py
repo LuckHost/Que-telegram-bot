02199bf383644e781c0162b4cded899e8f6fbe0e
LuckHost <sh76484@gmail.com>
2024-01-26 02:24:34 +0700

    Some buttons added

--- queue_bot.py
+++ queue_bot.py
@@ -2,7 +2,7 @@ import asyncio
 import logging
 
 from aiogram import Bot, Dispatcher, types, F
-from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup, KeyboardButton
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 from aiogram.filters import Command, StateFilter
 from aiogram.filters.state import State, StatesGroup
@@ -23,6 +23,7 @@ vvpd = {}
 class Dialog(StatesGroup):
     waiting_for_name = State()
     queue_entry = State()
+    task_await = State()
     talk = State()
 
 @dp.message(StateFilter(None), Command("start"))
@@ -37,7 +38,29 @@ async def start_message(messege: types.Message, state: FSMContext):
         await messege.answer("Напииши имя и фамилию")
         await state.set_state(Dialog.waiting_for_name)
         return
-    await messege.answer("Такой пупс уже найден")
+    await messege.answer("C возвращением!")
+    await state.set_state(Dialog.task_await)
+    
+@dp.message(Dialog.task_await)
+async def task_await(messege: types.Message, state: FSMContext):
+    """
+    
+    Gives a keayborad with the based commands.
+    
+    """
+    reply_keyboard = [
+            [
+                KeyboardButton(text="/sign_up"), 
+                KeyboardButton(text="/info")
+            ]
+            ]
+    base_commands_keyboard = ReplyKeyboardMarkup(keyboard=reply_keyboard,
+                                                     resize_keyboard=True,
+                                                     one_time_keyboard=True)
+        
+    await messege.answer(text="Выберете опцию...",
+                             reply_markup = base_commands_keyboard)
+    await state.set_state(None)
     
 @dp.message(StateFilter(None), Command("info"))
 async def show_base(messege: types.Message, state: FSMContext):
@@ -56,14 +79,20 @@ async def show_base(messege: types.Message, state: FSMContext):
     
 @dp.callback_query(F.data.startswith("que_"))
 async def send_random_value(callback: types.CallbackQuery):
+    """
+
+    Inline buttons implementation.
+
+    """
     action = callback.data.split("_")[1]
     if action == "inform":
         if callback.from_user.id not in informatics:
-            await 
-    elif action == "decr":
-        pass
-    elif action == "finish":
-        pass
+            
+            await callback.answer("Вы пытаетесь записаться на инфу")
+    elif action == "vvpd":
+        await callback.answer("Вы пытаетесь записаться на ввпд")
+    elif action == "op":
+        await callback.answer("Вы пытаетесь записаться на ОП")
 
     await callback.answer()
     
@@ -79,8 +108,8 @@ async def sign_up(messege: types.Message, state: FSMContext):
         return
     await state.set_state(Dialog.queue_entry)
     iform_but = InlineKeyboardButton(text = "Информатика", callback_data="que_inform")
-    vvpd_but = InlineKeyboardButton(text = "Информатика", callback_data="que_vvpd")
-    op_but = InlineKeyboardButton(text = "Информатика", callback_data="que_op")
+    vvpd_but = InlineKeyboardButton(text = "ВВПД", callback_data="que_vvpd")
+    op_but = InlineKeyboardButton(text = "ОП", callback_data="que_op")
     queue_keyboard = InlineKeyboardBuilder().add(iform_but, vvpd_but, op_but)
     await messege.answer("Выбери предмет", reply_markup=queue_keyboard.as_markup())
 
@@ -91,9 +120,9 @@ async def import_name(message: types.Message, state: FSMContext):
     Takes the name of user if it is not in base.
     
     """
-    await message.answer(f"Ваше имя: {message.text}")
+    await message.answer(f"Добро пожаловать, {message.text}!")
     users.update({message.from_user.id: f"{message.text}, ({message.from_user.full_name})"})
-    await state.set_state(None)
+    await state.set_state(Dialog.task_await)
 
 @dp.message(F.text)
 async def echo_message(message: types.Message, state: FSMContext):
